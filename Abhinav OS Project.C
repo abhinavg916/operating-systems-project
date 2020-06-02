2f963866a109124a10d27392bfe63f61334e26ab
Abhinav Dhiman <abhinavg916@gmail.com>
2020-06-02 13:11:38 +0530

    Update Abhinav OS Project.C

--- Abhinav OS Project.C
+++ Abhinav OS Project.C
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 
-/*<--------------MAIN FUNCTION--------------->*/
 int main() {
    int Max[10][10], need[10][10], alloc[10][10], avail[10], completed[10],
       safeSequence[10];
